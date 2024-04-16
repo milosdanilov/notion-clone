0d912d14a87262e6fa821f118af9964524a0be2f
Milos Danilov <milosdanilov@gmail.com>
2024-04-16 23:46:52 +0200

    chore: set spartan-ui

--- notion-clone-app/tailwind.config.cjs
+++ notion-clone-app/tailwind.config.cjs
@@ -1,8 +1,9 @@
 const { createGlobPatternsForDependencies } = require('@nx/angular/tailwind');
-const { join } = require('node:path');
+const { join } = require('path');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  presets: [require('@spartan-ng/ui-core/hlm-tailwind-preset')],
   content: [
     join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
     ...createGlobPatternsForDependencies(__dirname),
