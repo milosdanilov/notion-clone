67d03e20b51214af66a5556e3ac414db60b03dbd
Milos Danilov <milosdanilov@gmail.com>
2024-04-23 00:02:39 +0200

    feat: add title-section UI component

---
+++ notion-clone-app/src/libs/components/landing-page/title-section.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'nc-title-section',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './title-section.component.html',
+})
+export class TitleSectionComponent {
+
+  @Input()
+  pill!: string;
+
+  @Input()
+  subheading?: string;
+
+  @Input()
+  title?: string;
+}
