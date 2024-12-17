a4961a11c1715db8775c4505f500b5d8a29bd47e
Milos Danilov <milosdanilov@gmail.com>
2024-12-17 22:10:25 +0100

    style(site): format home.page

--- notion-clone-app/src/app/pages/(site)/(home).page.ts
+++ notion-clone-app/src/app/pages/(site)/(home).page.ts
@@ -40,188 +40,88 @@ import {
   selector: 'nc-site-home-page',
   template: `
     <section
-      class="overflow-hidden 
-      px-4 
-      sm:px-6 
-      mt-10 
-      sm:flex 
-      sm:flex-col 
-      gap-4 
-      md:justify-center 
-      md:items-center"
-    >
+      class="overflow-hidden px-4 sm:px-6 mt-10 sm:flex sm:flex-col gap-4 md:justify-center md:items-center">
       <nc-title-section
         pill="✨ Your Workspace, Perfected"
-        title="All-In-One Collaboration and Productivity Platform"
-      />
+        title="All-In-One Collaboration and Productivity Platform" />
       <div
-        class="bg-white 
-        p-[2px] 
-        mt-[6] 
-        rounded-xl 
-        bg-gradient-to-r 
-        from-primary 
-        to-brand-primaryBlue 
-        sm:w-[300px]"
-      >
+        class="bg-white p-[2px] mt-[6] rounded-xl bg-gradient-to-r from-primary to-brand-primaryBlue sm:w-[300px]">
         <button
           hlmBtn
           variant="secondary"
-          class="w-full 
-          rounded-[10px] 
-          p-6 
-          text-2xl 
-          bg-background"
+          class="w-full rounded-[10px] p-6 text-2xl bg-background"
           >Get Cypress Free</button
         >
       </div>
       <div
-        class="md:mt-[-90px] 
-        sm:w-full 
-        w-[750px] 
-        flex 
-        justify-center 
-        items-center 
-        mt-[-40px] 
-        relative 
-        sm:ml-0 
-        ml-[-50px]"
-      >
-        <img
-          src="assets/appBanner.png"
-          alt="Application Banner"
-        />
+        class="md:mt-[-90px] sm:w-full w-[750px] flex justify-center items-center mt-[-40px] relative sm:ml-0 ml-[-50px]">
+        <img src="assets/appBanner.png" alt="Application Banner" />
         <div
-          class="bottom-0 
-          top-[50%] 
-          bg-gradient-to-t 
-          dark:from-background 
-          left-0 
-          right-0 
-          absolute 
-          z-10"
-        >
-        </div>
+          class="bottom-0 top-[50%] bg-gradient-to-t dark:from-background left-0 right-0 absolute z-10"></div>
       </div>
     </section>
+
     <section class="relative">
       <nc-logo-carousel [logos]="clients" />
     </section>
+
     <section
-      class="px-4 
-      sm:px-6
-      flex
-      justify-center
-      items-center
-      flex-col
-      relative"
-    >
+      class="px-4 sm:px-6 flex justify-center items-center flex-col relative">
       <div
-        class="w-[30%]
-        blur-[120px]
-        rounded-full
-        h-32
-        absolute
-        bg-brand-primaryPurple/50
-        -z-10
-        top-22"
-      ></div>
+        class="w-[30%] blur-[120px] rounded-full h-32 absolute bg-brand-primaryPurple/50 -z-10 top-22"></div>
       <nc-title-section
         title="Keep track of your meetings, all in one place"
         subheading="Capture your ideas, thoughts, and meeting notes in a structured and organized manner."
-        pill="Features"
-      />
+        pill="Features" />
       <div
-        class="mt-10 
-        max-w-[450px] 
-        flex
-        justify-center
-        items-center
-        relative
-        sm:ml-0
-        rounded-2xl
-        border-8
-        border-washed-purple-300
-        border-opacity-10"
-      >
-        <img
-          src="assets/cal.png"
-          alt="Banner"
-          class="rounded-2xl"
-        />
+        class="mt-10 max-w-[450px] flex justify-center items-center relative sm:ml-0 rounded-2xl border-8 border-washed-purple-300 border-opacity-10">
+        <img src="assets/cal.png" alt="Banner" class="rounded-2xl" />
       </div>
     </section>
+
     <section class="relative">
       <div
-        class="w-full
-        blur-[120px]
-        rounded-full
-        h-32
-        absolute
-        bg-brand-primaryPurple/50
-        -z-10
-        top-56"
-      ></div>
+        class="w-full blur-[120px] rounded-full h-32 absolute bg-brand-primaryPurple/50 -z-10 top-56"></div>
       <div
-        class="mt-20
-        px-4
-        sm-px-6
-        flex
-        flex-col
-        overflow-x-hidden
-        overflow-visible"
-      >
+        class="mt-20 px-4 sm-px-6 flex flex-col overflow-x-hidden overflow-visible">
         <nc-title-section
           title="Trusted by all"
           subheading="Join thousands of satisfied users who rely on our platform for their personal and professional productivity needs."
-          pill="Testemonials"
-        />
+          pill="Testemonials" />
         @for (item of [1, 2]; track $index) {
         <div
-          class="mt-10
-          flex
-          flex-nowrap
-          gap-6
-          self-start
-          hover:paused"
+          class="mt-10 flex flex-nowrap gap-6 self-start hover:paused"
           [ngClass]="{
-          'flex-row-reverse': item === 2,
-          'animate-[slide_250s_linear_infinite]': true,
-          'animate-[slide_250s_linear_infinite_reverse]': item === 2,
-          'ml-[100vw]': item === 2,
-         }"
-        >
+            'flex-row-reverse': item === 2,
+            'animate-[slide_250s_linear_infinite]': true,
+            'animate-[slide_250s_linear_infinite_reverse]': item === 2,
+            'ml-[100vw]': item === 2,
+          }">
           @for (testemonial of users; track $index) {
           <nc-custom-card
-            class="w-[500px] shrink-0s rounded-xl dark:bg-gradient-to-t dark:from-border dark:to-background"
-          >
+            class="w-[500px] shrink-0s rounded-xl dark:bg-gradient-to-t dark:from-border dark:to-background">
             <div ncCustomCardHeader>
               <div class="flex items-center gap-4">
                 <hlm-avatar>
                   <img
                     hlmAvatarImage
-                    src="{{ 'assets/avatars/' + ($index + 1) + '.png' }}"
-                  />
+                    src="{{ 'assets/avatars/' + ($index + 1) + '.png' }}" />
                   <span hlmAvatarFallback>AV</span>
                 </hlm-avatar>
                 <div>
-                  <div
-                    hlmCardTitle
-                    class="text-foreground"
-                    >{{ testemonial.name }}</div
-                  >
-                  <div
-                    hlmCardDescription
-                    class="dark:text-washed-purple-800"
-                    >{{ testemonial.name | lowercase }}</div
-                  >
+                  <div hlmCardTitle class="text-foreground">
+                    {{ testemonial.name }}
+                  </div>
+                  <div hlmCardDescription class="dark:text-washed-purple-800">
+                    {{ testemonial.name | lowercase }}
+                  </div>
                 </div>
               </div>
             </div>
             <div ncCustomCardContent>
-              <p class="dark:text-washed-purple-800">{{
-                testemonial.message
-              }}</p>
+              <p class="dark:text-washed-purple-800">
+                {{ testemonial.message }}
+              </p>
             </div>
           </nc-custom-card>
           }
@@ -229,55 +129,42 @@ import {
         }
       </div>
     </section>
+
     <section class="mt-20 px-4 sm:px-6">
       <nc-title-section
         title="The Perfect Plan For You"
         subheading="Experience all the benefits of our platform. Select a plan that suits your needs and take your productivity to new heights."
-        pill="Pricing"
-      />
+        pill="Pricing" />
       <div
-        class="flex flex-col-reverse sm:flex-row gap-4 justify-center sm:items-stretch items-center mt-10"
-      >
+        class="flex flex-col-reverse sm:flex-row gap-4 justify-center sm:items-stretch items-center mt-10">
         @for (card of pricingCards; track $index) {
         <nc-custom-card
           class="w-[300px] rounded-2xl dark:bg-black/40 background-blur-3xl relative"
           [ngClass]="{
             'border-brand-primaryPurple/70':
               card.planType === pricingPlans.proplan
-          }"
-        >
+          }">
           <div ncCustomCardHeader>
-            <div
-              hlmCardTitle
-              class="text-2xl font-semibold"
-            >
+            <div hlmCardTitle class="text-2xl font-semibold">
               @if (card.planType === pricingPlans.proplan) {
               <div
-                class="hidden dark:block w-full blur-[120px] rounded-full h-32 absolute bg-brand-primaryPurple/80 -z-10 top-0"
-              >
+                class="hidden dark:block w-full blur-[120px] rounded-full h-32 absolute bg-brand-primaryPurple/80 -z-10 top-0">
               </div>
               <img
                 src="assets/icons/diamond.svg"
                 alt="Pro Plan Icon"
-                class="absolute top-6 right-6"
-              />
+                class="absolute top-6 right-6" />
               }
               {{ card.planType }}
             </div>
           </div>
-          <div
-            ncCustomCardContent
-            class="p-0"
-          >
+          <div ncCustomCardContent class="p-0">
             <span class="font-normal text-2xl">\${{ card.price }}</span>
             @if (+card.price > 0) {
             <span class="dark:text-washed-purple-800 ml-1">/mo</span>
             }
             <p class="dark:text-washed-purple-800">{{ card.description }}</p>
-            <button
-              variant="btn-primary"
-              class="whitespace-nowrap w-full mt-4"
-            >
+            <button variant="btn-primary" class="whitespace-nowrap w-full mt-4">
               {{
                 card.planType === pricingPlans.proplan
                   ? 'Go Pro'
@@ -290,10 +177,7 @@ import {
               <small>{{ card.highlightFeature }}</small>
               @for (feature of card.freatures; track $index) {
               <li class="flex items-center gap-2">
-                <img
-                  src="assets/icons/check.svg"
-                  alt="Check Icon"
-                />
+                <img src="assets/icons/check.svg" alt="Check Icon" />
                 {{ feature }}
               </li>
               }
