75419cc32e4483046d91c406c39e2388125d401d
Milos Danilov <milosdanilov@gmail.com>
2025-01-29 19:14:40 +0100

    test(ui/utils): fix test for disable ctrl

--- libs/ui/ui-utils/src/lib/disable-ctrl/disable-ctrl.directive.spec.ts
+++ libs/ui/ui-utils/src/lib/disable-ctrl/disable-ctrl.directive.spec.ts
@@ -1,8 +1,26 @@
+import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+
+import { render } from '@testing-library/angular';
+
 import { DisableCtrlDirective } from './disable-ctrl.directive';
 
+@Component({
+  selector: 'ui-test-disable-ctrl-input-mock',
+  imports: [ReactiveFormsModule, DisableCtrlDirective],
+  template: `
+    <input type="text" [formControl]="testCtrl" [uiDisableCtrl]="disabled" />
+  `,
+  standalone: true,
+})
+class DisableCtrlInputMockComponent {
+  testCtrl = new FormControl('');
+  disabled = false;
+}
+
 describe('DisableCtrlDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DisableCtrlDirective();
-    expect(directive).toBeTruthy();
+  it('should create an instance', async () => {
+    const comp = await render(DisableCtrlInputMockComponent);
+    expect(comp.fixture.componentInstance).toBeTruthy();
   });
 });
