62cb8b42b4e557c795555c69aaab0fabd4055bdb
Milos Danilov <milosdanilov@gmail.com>
2025-02-03 23:16:15 +0100

    feat(ui/emoji-picker): convert to custom form element

--- libs/ui/emoji-picker/src/lib/emoji-picker/emoji-picker.component.ts
+++ libs/ui/emoji-picker/src/lib/emoji-picker/emoji-picker.component.ts
@@ -1,5 +1,11 @@
-import { Component, output } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import {
+  ControlValueAccessor,
+  FormsModule,
+  NG_VALUE_ACCESSOR,
+} from '@angular/forms';
 
 import { PickerComponent } from '@ctrl/ngx-emoji-mart';
 import { EmojiData, EmojiEvent } from '@ctrl/ngx-emoji-mart/ngx-emoji';
@@ -9,7 +15,10 @@ import {
   BrnPopoverContentDirective,
   BrnPopoverTriggerDirective,
 } from '@spartan-ng/brain/popover';
+
 import { HlmPopoverContentDirective } from '@spartan-ng/ui-popover-helm';
+
+import { NOOP } from '@/utils';
 
 export type Emoji = EmojiData;
 
@@ -23,24 +32,63 @@ export type Emoji = EmojiData;
     BrnPopoverTriggerDirective,
     BrnPopoverContentDirective,
     HlmPopoverContentDirective,
+    FormsModule,
   ],
   template: `
     <div class="flex items-center">
       <brn-popover>
         <button brnPopoverTrigger type="button">
-          <ng-content></ng-content>
+          {{ emoji }}
         </button>
-        <div class="p-0 border-none" hlmPopoverContent *brnPopoverContent>
-          <emoji-mart (emojiClick)="selectEmoji($event)" />
+        <div
+          class="p-0 border-none"
+          hlmPopoverContent
+          *brnPopoverContent="let ctx">
+          <emoji-mart
+            (emojiSelect)="selectEmoji($event)"
+            [enableSearch]="false" />
         </div>
       </brn-popover>
+      <input
+        type="hidden"
+        [attr.name]="name()"
+        [ngModel]="emoji"
+        [ngModelOptions]="{ standalone: true }" />
     </div>
   `,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: EmojiPickerComponent,
+    },
+  ],
 })
-export class EmojiPickerComponent {
-  emojiChange = output<string>();
+export class EmojiPickerComponent implements ControlValueAccessor {
+  protected emoji = '';
+
+  onChange: (emoji: string) => void = NOOP;
+
+  onTouched: () => void = NOOP;
+
+  name = input.required<string>();
 
   selectEmoji(event: EmojiEvent) {
-    this.emojiChange.emit(event.emoji.native || event.emoji.text);
+    this.emoji = event.emoji.native || event.emoji.text;
+    this.onTouched();
+
+    this.onChange(this.emoji);
+  }
+
+  writeValue(emoji: string): void {
+    this.emoji = emoji;
+  }
+
+  registerOnChange(fn: (emoji: string) => void): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
   }
 }
