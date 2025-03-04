dab256241a7033d27063ad839c7368fd36ed2e99
Milos Danilov <milosdanilov@gmail.com>
2025-03-04 14:49:59 +0100

    feat(dashboard): add workspace layout page

--- notion-clone-app/src/app/pages/(main)/dashboard/[workspaceId].page.ts
+++ notion-clone-app/src/app/pages/(main)/dashboard/[workspaceId].page.ts
@@ -1,7 +1,19 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
+  selector: 'nc-workspace-layout-page',
   standalone: true,
-  template: `<div>IT WORKS</div>`,
+  imports: [RouterOutlet],
+  template: `
+    <main class="flex overflow-hidden h-screen w-screen">
+      <!-- <nc-sidebar></nc-sidebar> -->
+      <div
+        class="dark:border-Neutrals/neutrals-12/70 
+        border-l-[1px] w-full relative overflow-scroll">
+        <router-outlet />
+      </div>
+    </main>
+  `,
 })
-export default class WorkspaceIdPageComponent {}
+export default class WorkspacePageLayoutComponent {}
