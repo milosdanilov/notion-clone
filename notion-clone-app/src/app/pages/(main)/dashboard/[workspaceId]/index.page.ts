7c90a81be61cd1655ef9f43dedb1ee9aec973c98
Milos Danilov <milosdanilov@gmail.com>
2025-03-04 14:50:16 +0100

    feat(dashboard): add empty workspace page

---
+++ notion-clone-app/src/app/pages/(main)/dashboard/[workspaceId]/index.page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nc-workspace-page',
+  standalone: true,
+  template: `<h1>Workspace Id page</h1>`,
+})
+export default class WorkspacePageComponent {}
