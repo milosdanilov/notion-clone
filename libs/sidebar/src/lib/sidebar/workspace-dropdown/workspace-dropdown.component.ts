1851aa34ccc3db3476903d5d6785700b1d61ab44
Milos Danilov <milosdanilov@gmail.com>
2025-03-11 12:21:14 +0100

    feat(sidebar): add dialog trigger to create new workspaces

--- libs/sidebar/src/lib/sidebar/workspace-dropdown/workspace-dropdown.component.ts
+++ libs/sidebar/src/lib/sidebar/workspace-dropdown/workspace-dropdown.component.ts
@@ -1,13 +1,36 @@
-import { Component, computed, effect, input, signal } from '@angular/core';
+import { Component, effect, input, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { Workspace } from '@notion-clone/workspace/server';
 
 import { SelectedWorkspaceComponent } from '../selected-workspace/selected-workspace.component';
+import {
+  HlmDialogComponent,
+  HlmDialogContentComponent,
+  HlmDialogDescriptionDirective,
+  HlmDialogHeaderComponent,
+  HlmDialogTitleDirective,
+} from '@spartan-ng/ui-dialog-helm';
+import {
+  BrnDialogContentDirective,
+  BrnDialogTriggerDirective,
+} from '@spartan-ng/brain/dialog';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
 
 @Component({
   selector: 'lib-workspace-dropdown',
-  imports: [CommonModule, SelectedWorkspaceComponent],
+  imports: [
+    CommonModule,
+    SelectedWorkspaceComponent,
+    HlmDialogComponent,
+    BrnDialogTriggerDirective,
+    HlmButtonDirective,
+    HlmDialogContentComponent,
+    BrnDialogContentDirective,
+    HlmDialogHeaderComponent,
+    HlmDialogTitleDirective,
+    HlmDialogDescriptionDirective,
+  ],
   template: `
     <div class="relative inline-block text-left">
       <div>
@@ -56,6 +79,22 @@ import { SelectedWorkspaceComponent } from '../selected-workspace/selected-works
                   "></ng-container>
               }
             </div>
+            <hlm-dialog>
+              <button brnDialogTrigger hlmBtn>Button</button>
+              <hlm-dialog-content
+                *brnDialogContent="let ctx"
+                class="h-screen block sm:h-[440px] overflow-scroll w-full">
+                <hlm-dialog-header>
+                  <h3 hlmDialogTitle>Create a Workspace</h3>
+                  <p hlmDialogDescription
+                    >Workspaces give you the power to collaborate with others.
+                    You can change your workspace privacy settings after
+                    creating the workspace too.</p
+                  >
+                  <div>WorkspaceCreator</div>
+                </hlm-dialog-header>
+              </hlm-dialog-content>
+            </hlm-dialog>
           </div>
         </div>
       }
