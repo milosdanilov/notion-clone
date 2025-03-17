2db4addf9cbc500b3f23e89d2ea583da255877ed
Milos Danilov <milosdanilov@gmail.com>
2025-03-17 21:10:25 +0100

    fix(migrations): properly create subscriptions

--- libs/supabase/migrations/schema.ts
+++ libs/supabase/migrations/schema.ts
@@ -35,23 +35,52 @@ export const prices = pgTable("prices", {
 	metadata: jsonb("metadata"),
 });
 
-export const subscriptions = pgTable("subscriptions", {
-	id: text("id").primaryKey().notNull(),
-	userId: uuid("user_id").notNull().references(() => users.id).references(() => users.id),
-	status: subscriptionStatus("status"),
-	metadata: jsonb("metadata"),
-	priceId: text("price_id").references(() => prices.id).references(() => prices.id),
-	quantity: integer("quantity"),
-	cancelAtPeriodEnd: boolean("cancel_at_period_end"),
-	created: timestamp("created", { withTimezone: true, mode: 'string' }).default(sql`now()`).notNull(),
-	currentPeriodStart: timestamp("current_period_start", { withTimezone: true, mode: 'string' }).default(sql`now()`).notNull(),
-	currentPeriodEnd: timestamp("current_period_end", { withTimezone: true, mode: 'string' }).default(sql`now()`).notNull(),
-	endedAt: timestamp("ended_at", { withTimezone: true, mode: 'string' }).default(sql`now()`),
-	cancelAt: timestamp("cancel_at", { withTimezone: true, mode: 'string' }).default(sql`now()`),
-	canceledAt: timestamp("canceled_at", { withTimezone: true, mode: 'string' }).default(sql`now()`),
-	trialStart: timestamp("trial_start", { withTimezone: true, mode: 'string' }).default(sql`now()`),
-	trialEnd: timestamp("trial_end", { withTimezone: true, mode: 'string' }).default(sql`now()`),
-});
+export const subscriptions = pgTable('subscriptions', {
+	id: text('id').primaryKey().notNull(),
+	userId: uuid('user_id')
+	  .notNull()
+	  .references(() => users.id),
+	status: subscriptionStatus('status'),
+	metadata: jsonb('metadata'),
+	priceId: text('price_id').references(() => prices.id),
+	quantity: integer('quantity'),
+	cancelAtPeriodEnd: boolean('cancel_at_period_end'),
+	created: timestamp('created', { withTimezone: true, mode: 'string' })
+	  .default(sql`now()`)
+	  .notNull(),
+	currentPeriodStart: timestamp('current_period_start', {
+	  withTimezone: true,
+	  mode: 'string',
+	})
+	  .default(sql`now()`)
+	  .notNull(),
+	currentPeriodEnd: timestamp('current_period_end', {
+	  withTimezone: true,
+	  mode: 'string',
+	})
+	  .default(sql`now()`)
+	  .notNull(),
+	endedAt: timestamp('ended_at', {
+	  withTimezone: true,
+	  mode: 'string',
+	}).default(sql`now()`),
+	cancelAt: timestamp('cancel_at', {
+	  withTimezone: true,
+	  mode: 'string',
+	}).default(sql`now()`),
+	canceledAt: timestamp('canceled_at', {
+	  withTimezone: true,
+	  mode: 'string',
+	}).default(sql`now()`),
+	trialStart: timestamp('trial_start', {
+	  withTimezone: true,
+	  mode: 'string',
+	}).default(sql`now()`),
+	trialEnd: timestamp('trial_end', {
+	  withTimezone: true,
+	  mode: 'string',
+	}).default(sql`now()`),
+  });
 
 export const users = pgTable("users", {
 	id: uuid("id").primaryKey().notNull(),
