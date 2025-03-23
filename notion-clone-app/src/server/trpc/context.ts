34f29769dcc3b879a65339be58de7849f73648bc
Milos Danilov <milosdanilov@gmail.com>
2025-03-23 19:44:20 +0100

    feat(trpc): setup trpc

--- notion-clone-app/src/server/trpc/context.ts
+++ notion-clone-app/src/server/trpc/context.ts
@@ -1,7 +1,19 @@
+import { createAuthClient } from '@/utils';
 import { inferAsyncReturnType } from '@trpc/server';
+import { H3Event } from 'h3';
 /**
  * Creates context for an incoming request
  * @link https://trpc.io/docs/context
  */
-export const createContext = () => ({});
+export const createContext = async (event: H3Event) => {
+  const authClient = createAuthClient(event);
+
+  const {
+    data: { user },
+  } = await authClient.getUser();
+
+  return {
+    user,
+  };
+};
 export type Context = inferAsyncReturnType<typeof createContext>;
