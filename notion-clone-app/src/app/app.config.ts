ba58576f2119f3cf8f6f50904fdd7e87f6f58174
Milos Danilov <milosdanilov@gmail.com>
2025-03-25 15:01:26 +0100

    style(app): import reorder

--- notion-clone-app/src/app/app.config.ts
+++ notion-clone-app/src/app/app.config.ts
@@ -1,10 +1,10 @@
 import { ApplicationConfig } from '@angular/core';
+import { withComponentInputBinding } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideFileRouter } from '@analogjs/router';
 
 import { provideTrpcClient } from '../trpc-client';
-import { withComponentInputBinding } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
